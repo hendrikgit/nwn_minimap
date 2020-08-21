abf8804ae0fc43934465fb87a5004f32ac4a2286
Hendrik Albers <mail@hendrik.dev>
2020-08-21 23:27:19 +0200

    clear wand, get/reset iterator, appendImages, addImage

--- src/magickwand.nim
+++ src/magickwand.nim
@@ -1,7 +1,7 @@
 type
-  MagickWand {.importc: "struct MagickWand".} = object
+  MagickWand = object
 
-  PixelWand {.importc: "struct PixelWand".} = object
+  PixelWand = object
 
   FilterType = enum
     LanczosFilter
@@ -9,7 +9,7 @@ type
   Wand* = object
     impl: ptr MagickWand
 
-{.push dynlib: "libMagickWand-6.Q16.so".}
+{.push dynlib: "libMagickWand-6(.Q16).so(.6)".}
 proc genesis*() {.importc: "MagickWandGenesis".}
 
 proc terminus*() {.importc: "MagickWandTerminus".}
@@ -17,6 +17,8 @@ proc terminus*() {.importc: "MagickWandTerminus".}
 proc newMagickWand(): ptr MagickWand {.importc: "NewMagickWand".}
 
 proc destroyMagickWand(wand: ptr MagickWand): ptr MagickWand {.importc: "DestroyMagickWand", discardable.}
+
+proc clearMagickWand(wand: ptr MagickWand) {.importc: "ClearMagickWand".}
 
 proc magickReadImage(wand: ptr MagickWand, filename: cstring): bool {.importc: "MagickReadImage".}
 
@@ -33,6 +35,14 @@ proc destroyPixelWand(wand: ptr PixelWand): ptr PixelWand {.importc: "DestroyPix
 proc magickRotateImage(wand: ptr MagickWand, background: ptr PixelWand, degrees: cdouble): bool {.importc: "MagickRotateImage".}
 
 proc magickResizeImage(wand: ptr MagickWand, columns, rows: csize_t, filter: FilterType): bool {.importc: "MagickResizeImage".}
+
+proc magickAppendImages(wand: ptr MagickWand, stack: bool): ptr MagickWand {.importc: "MagickAppendImages".}
+
+proc magickAddImage(wand, addWand: ptr MagickWand): bool {.importc: "MagickAddImage".}
+
+proc magickResetIterator(wand: ptr MagickWand) {.importc: "MagickResetIterator".}
+
+proc magickGetIteratorIndex(wand: ptr MagickWand): int {.importc: "MagickGetIteratorIndex".}
 {.pop.}
 
 proc newWand*(): Wand =
@@ -40,6 +50,9 @@ proc newWand*(): Wand =
 
 proc `=destroy`(wand: var Wand) =
   wand.impl = destroyMagickWand(wand.impl)
+
+proc clearWand*(wand: Wand) =
+  clearMagickWand(wand.impl)
 
 proc readImage*(wand: Wand, filename: string) =
   if not magickReadImage(wand.impl, filename):
@@ -58,12 +71,24 @@ proc height*(wand: Wand): uint =
 proc size*(wand: Wand): tuple[width, height: uint] =
   (wand.width, wand.height)
 
-proc rotateImage*(wand: Wand, degrees: float64) =
+proc rotateImage*(wand: Wand, degrees: int) =
   var pw = newPixelWand()
   # todo: use return code for error handling?
-  discard magickRotateImage(wand.impl, pw, degrees)
+  discard magickRotateImage(wand.impl, pw, degrees.toFloat)
   discard pw.destroyPixelWand
 
 proc resizeImage*(wand: Wand, columns, rows: uint) =
   # todo: return code?
   discard magickResizeImage(wand.impl, columns, rows, LanczosFilter)
+
+proc appendImages*(wand: Wand, stack = false): Wand =
+  result.impl = magickAppendImages(wand.impl, stack)
+
+proc addImage*(wand, addWand: Wand) =
+  discard magickAddImage(wand.impl, addWand.impl)
+
+proc resetIterator*(wand: Wand) =
+  magickResetIterator(wand.impl)
+
+proc getIteratorIndex*(wand: Wand): int =
+  magickGetIteratorIndex(wand.impl)
